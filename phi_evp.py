b32edc122b2194016fa1e817cee35360a68b33bc
Liam O'Connor <liamoconnor2025@u.northwestern.edu>
2022-05-19 12:33:58 -0500

    dist fourier

--- phi_evp.py
+++ phi_evp.py
@@ -23,33 +23,44 @@ def construct_phi(a, delta, dist, coords, bases):
     Nx = max(x.shape)
     Ny = max(y.shape)
 
+    a0 = -2.0
+    pt = 0
+    a = [a0 - pt, 1.0, 0.4, 1.4, 0.24, 0.124, 0.9]
+
     #ellipse
     n = len(a)
-    ks = list(range(n))
-    
-    theta = np.pi
+    ks = [0]
+    for i in range(2, n + 1):
+        ks.append((-1)**i * int(i / 2))    
 
-    dist_mat = np.zeros((n, n))
-    for row in range(n):
-        for col in range(n):
-            if (row == col):
-                dist_mat[row, col] = a[row] * np.conj(a[row])
-            elif (col > row):
-                dist_mat[row, col] = a[col] * np.conj(a[row]) * np.exp(1j*col*theta)
-            else:
-                dist_mat[row, col] = np.conj(a[col] * np.conj(a[row]) * np.exp(1j*col*theta))
-
-    print(np.sqrt(np.sum(dist_mat)))
-    sys.exit()
-
-    
-    # ks = [0]
-    # for i in range(2, n + 1):
-    #     ks.append((-1)**i * int(i / 2))
-
+    # theta = np.pi
+    def dist(thetas, a):
+        n = len(a)
         
-    thetas = np.linspace(0, 2*np.pi, 1000)
+        dist_mat = np.zeros((n, n, len(thetas)), dtype=np.complex128)
+        for row in range(n):
+            for col in range(n):
+                dist_mat[row, col, :] = (a[col] * np.conj(a[row]) * np.exp(1j*(ks[col]-ks[row])*thetas))
 
+        return dist_mat.sum(axis=tuple(range(dist_mat.ndim - 1))).real
+
+    def dist_prime(thetas, a):
+        n = len(a)
+        dist_mat = np.zeros((n, n, len(thetas)), dtype=np.complex128)
+        # for col in range(n):
+        #     for row in range(n):
+        for col in range(1, n):
+            for row in range(col):
+                dist_mat[row, col, :] = -2*np.imag((ks[col]-ks[row])*(a[col] * np.conj(a[row]) * np.exp(1j*(ks[col]-ks[row])*thetas)))
+
+        return dist_mat.sum(axis=tuple(range(dist_mat.ndim - 1))).real
+
+    sin_coeff = np.zeros(n)
+    cos_coeff = np.zeros(n)
+
+    thetas = np.linspace(-0.1, 2*np.pi+0.1, 200)
+    dists = dist(thetas, a)
+ 
     r = np.zeros(thetas.shape, dtype=np.complex128)
     for i, theta in enumerate(thetas):
         for k, a_k in zip(ks, a):
@@ -58,6 +69,30 @@ def construct_phi(a, delta, dist, coords, bases):
     rx = r.real
     ry = r.imag
     rs = list(zip(rx, ry))
+
+    thetas_adj = np.arctan2(ry, rx)
+    plt.plot(thetas, dists)
+    dist_primes = dist_prime(thetas, a)
+    plt.plot(thetas, dist_primes)
+    # plt.scatter(dists*np.cos(thetas_adj), dists*np.sin(thetas_adj))
+
+    # plt.axvline(x=np.arctan(1/2)) 
+    # plt.axhline(y=0) 
+    plt.show()
+    plt.close()
+    sys.exit()
+
+    # dists_comp = dists * np.exp(1j*thetas_adj)
+
+    rx2 = dists * np.cos(thetas_adj)
+    ry2 = dists * np.sin(thetas_adj)
+    # rx2 = [dist*np.cos(theta) for dist, theta in zip(dists, thetas)]
+    # ry2 = [dist*np.sin(theta) for dist, theta in zip(dists, thetas)]
+
+    plt.scatter(rx, ry)
+    plt.scatter(rx2, ry2, marker='.')
+    plt.show()
+    sys.exit()
 
     logger.info('solving for the signed distance function. This might take a sec')
     from matplotlib import path
@@ -91,7 +126,7 @@ tau = config.getfloat('parameters', 'tau')
 delta = config.getfloat('parameters', 'delta')
 
 scale = 1.0
-rotation = config.getfloat('parameters', 'rotation')
+rotation = 0.0
 
 max_timestep = config.getfloat('parameters', 'max_dt')
 stop_sim_time = config.getfloat('parameters', 'T') + 0.1
